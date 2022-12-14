b6167e582cca13c6a23f5ebe3b5ebf348f7ae2eb
Vaclav Elias <vaclavelias@gmail.com>
2022-12-14 10:41:10 +0100

    feat: DepartmentEndpoint moved to separate file

---
+++ src/ApiBureau.Bullhorn.Api/Endpoints/DepartmentEndpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiBureau.Bullhorn.Api.Endpoints;
+
+public class DepartmentEndpoint : BaseEndpoint
+{
+    public DepartmentEndpoint(ApiConnection apiConnection) : base(apiConnection) { }
+
+    /// <summary>
+    /// Returns all departments
+    /// </summary>
+    /// <returns></returns>
+    public Task<List<DepartmentDto>> GetAsync()
+        => ApiConnection.QueryAsync<DepartmentDto>("Department?fields=id,description,enabled,name&where=id>0");
+}
