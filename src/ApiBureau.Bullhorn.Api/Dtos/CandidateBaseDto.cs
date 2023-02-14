60948e33f36cfb748089d0b0f32deed87ca994d5
Vaclav Elias <vaclavelias@gmail.com>
2023-02-14 11:37:38 +0000

    fix: Add nullable for Source

--- src/ApiBureau.Bullhorn.Api/Dtos/CandidateBaseDto.cs
+++ src/ApiBureau.Bullhorn.Api/Dtos/CandidateBaseDto.cs
@@ -17,7 +17,7 @@ public class CandidateBaseDto : EntityBaseDto
     public bool IsDeleted { get; set; }
     public string Status { get; set; } = null!;
     public long CustomDate2 { get; set; }
-    public string Source { get; set; }
+    public string? Source { get; set; }
     public UserDto Owner { get; set; }
     public AddressDto Address { get; set; }
 
