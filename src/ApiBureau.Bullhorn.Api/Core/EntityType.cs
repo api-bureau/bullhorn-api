8cd94a55bb02172d81f7e5fa3b12b0420bac8ed8
Vaclav Elias <vaclavelias@gmail.com>
2023-04-19 18:59:37 +0200

    feat:  Types added to xml description

--- src/ApiBureau.Bullhorn.Api/Core/EntityType.cs
+++ src/ApiBureau.Bullhorn.Api/Core/EntityType.cs
@@ -1,5 +1,9 @@
 namespace ApiBureau.Bullhorn.Api.Core;
 
+/// <summary>
+/// Represents different types of entities.
+/// Possible values: Appointment, Candidate, ClientContact, ClientCorporation, Country, Department, JobOrder, JobSubmission, Note, Placement, PlacementChangeRequest, Tearsheet
+/// </summary>
 public enum EntityType
 {
     Appointment,
