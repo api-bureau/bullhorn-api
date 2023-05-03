044c8d34b61c785562971242d6eb152ff6897933
Vaclav Elias <vaclavelias@gmail.com>
2023-05-03 14:27:05 +0200

    feat: ToUtcDateTime() added

--- src/ApiBureau.Bullhorn.Api/Extensions/DateTimeExtension.cs
+++ src/ApiBureau.Bullhorn.Api/Extensions/DateTimeExtension.cs
@@ -5,6 +5,9 @@ public static class DateTimeExtension
     public static DateTime ToDateTime(this long timestamp)
         => DateTimeOffset.FromUnixTimeMilliseconds(timestamp).DateTime;
 
+    public static DateTime ToUtcDateTime(this long timestamp)
+        => DateTimeOffset.FromUnixTimeMilliseconds(timestamp).UtcDateTime;
+
     public static long Timestamp(this DateTime datetime)
         => new DateTimeOffset(datetime).ToUnixTimeMilliseconds();
 }
