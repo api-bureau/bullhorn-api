f3cb8327f45a583d76dbc43ae6850469154eb481
Vaclav Elias <vaclavelias@gmail.com>
2024-03-27 13:59:37 +0100

    refactor: XML comment added

--- src/ApiBureau.Bullhorn.Api/Dtos/FileDto.cs
+++ src/ApiBureau.Bullhorn.Api/Dtos/FileDto.cs
@@ -3,6 +3,10 @@ namespace ApiBureau.Bullhorn.Api.Dtos;
 public class FileDto
 {
     public string ContentType { get; set; } = "";
+
+    /// <summary>
+    /// Returns an attached file as base64-encoded text
+    /// </summary>
     public string FileContent { get; set; } = "";
     public string Name { get; set; } = "";
 }
