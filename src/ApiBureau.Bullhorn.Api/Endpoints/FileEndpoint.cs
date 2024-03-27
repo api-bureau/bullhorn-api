1c6d0244ed420f7e6b4b64f0884aebef44477c3f
Vaclav Elias <vaclavelias@gmail.com>
2024-03-27 18:14:28 +0100

    feat: GetFileAsync made obsolete

--- src/ApiBureau.Bullhorn.Api/Endpoints/FileEndpoint.cs
+++ src/ApiBureau.Bullhorn.Api/Endpoints/FileEndpoint.cs
@@ -4,6 +4,18 @@ public class FileEndpoint : BaseEndpoint
 {
     public FileEndpoint(ApiConnection apiConnection, string requestUrl) : base(apiConnection, requestUrl) { }
 
+    public async Task<FileDto?> GetFileAsync(EntityType entityType, int entityId, int fileId)
+    {
+        var query = $"{RequestUrl}/{entityType}/{entityId}/{fileId}?";
+
+        var response = await ApiConnection.GetAsync(query);
+
+        var fileResponse = await response.DeserializeAsync<FileResponse<FileDto>>();
+
+        return fileResponse?.File;
+    }
+
+    [Obsolete]
     public async Task<FileDto?> GetFileAsync(string entityType, int entityId, int fileId)
     {
         //ToDo refactor and use EntityType
