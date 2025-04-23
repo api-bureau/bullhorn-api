93a15a7803d026392bb4c4b3813ef078ce834cdf
Vaclav Elias <vaclavelias@gmail.com>
2025-04-23 16:05:06 +0100

    feat: ErrorResponse.cs - Add error details support
    - Modified the `ErrorResponse` class to include a new property, `Errors`, which is a nullable list of `ErrorDetail` objects.
    - Introduced a new class named `ErrorDetail` with properties for `PropertyName`, `Severity`, `DetailMessage`, and `Type`.
    - This change allows for the representation of multiple error details associated with the response, enhancing error handling capabilities.

--- src/ApiBureau.Bullhorn.Api/Http/ErrorResponse.cs
+++ src/ApiBureau.Bullhorn.Api/Http/ErrorResponse.cs
@@ -5,5 +5,14 @@ public class ErrorResponse
     public string ErrorMessage { get; set; } = "";
     public string ErrorMessageKey { get; set; } = "";
     public int ErrorCode { get; set; }
+    public List<ErrorDetail>? Errors { get; set; }
     public bool Success => string.IsNullOrWhiteSpace(ErrorMessage) && ErrorCode == 0;
+}
+
+public class ErrorDetail
+{
+    public string? PropertyName { get; set; }
+    public string? Severity { get; set; }
+    public string? DetailMesssage { get; set; }
+    public string? Type { get; set; }
 }
