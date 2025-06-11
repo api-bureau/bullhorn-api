a9bfb69946b7657acd78a484822878bb67e9c737
Vaclav Elias <vaclavelias@gmail.com>
2025-06-11 09:29:04 +0100

    feat: User entity added

--- src/ApiBureau.Bullhorn.Api/Core/EntityType.cs
+++ src/ApiBureau.Bullhorn.Api/Core/EntityType.cs
@@ -2,7 +2,7 @@ namespace ApiBureau.Bullhorn.Api.Core;
 
 /// <summary>
 /// Represents different types of entities.
-/// Possible values: Appointment, Candidate, ClientContact, ClientCorporation, Country, Department, JobOrder, JobSubmission, Lead, Note, Placement, PlacementCommision, PlacementChangeRequest, Tearsheet
+/// Possible values: Appointment, Candidate, ClientContact, ClientCorporation, Country, Department, JobOrder, JobSubmission, Lead, Note, Placement, PlacementCommission, PlacementChangeRequest, Tearsheet
 /// </summary>
 public enum EntityType
 {
@@ -21,5 +21,6 @@ public enum EntityType
     Placement,
     PlacementCommission,
     PlacementChangeRequest,
-    Tearsheet
+    Tearsheet,
+    User
 }
