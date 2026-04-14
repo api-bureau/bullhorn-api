7f9a4354863099b2ebe69790455982386885f7c2
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2026-04-14 12:08:23 +0100

    feat: MassUpdateEndpoint.cs - Add mass update GET methods
    - Add GetAsync(CancellationToken) to list entity types supporting mass update
    - Add GetAsync(EntityType, CancellationToken) to list updatable properties and entitlements for a given entity type
    - Add XML documentation comments for all public methods
    - Improve API clarity and usability for mass update operations

--- src/ApiBureau.Bullhorn.Api/Endpoints/MassUpdateEndpoint.cs
+++ src/ApiBureau.Bullhorn.Api/Endpoints/MassUpdateEndpoint.cs
@@ -4,6 +4,31 @@ public class MassUpdateEndpoint : EndpointBase
 {
     public MassUpdateEndpoint(ApiConnection apiConnection, string requestUrl) : base(apiConnection, requestUrl) { }
 
+    /// <summary>
+    /// Returns the list of entity types that support mass update.
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public Task<HttpResponseMessage> GetAsync(CancellationToken token) => ApiConnection.GetAsync($"/{RequestUrl}", token);
+
+    /// <summary>
+    /// Returns the list of entity properties for which mass update is supported on the specified entity type. Also
+    /// returns the entitlement required for updating each property.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public Task<HttpResponseMessage> GetAsync(EntityType type, CancellationToken token)
+        => ApiConnection.GetAsync($"/{RequestUrl}/{type}", token);
+
+    /// <summary>
+    /// Performs a massUpdate on all entity records of the specified type for which the entity id is included in the
+    /// request body.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="data"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
     public Task<HttpResponseMessage> UpdateAsync(EntityType type, object data, CancellationToken token)
         => ApiConnection.PostAsJsonAsync($"/{RequestUrl}/{type}", data, token);
 }
