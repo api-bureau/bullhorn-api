38f2e3d702d684a3f4c29c87949da6980280badc
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2026-05-19 11:56:48 +0100

    example: Program.cs - updateService commented out

--- src/ApiBureau.Bullhorn.Api.Console/Program.cs
+++ src/ApiBureau.Bullhorn.Api.Console/Program.cs
@@ -10,10 +10,12 @@ startup.ConfigureServices(services);
 
 var serviceProvider = services.BuildServiceProvider();
 
-//var service = serviceProvider.GetService<UpdateFieldService>();
+var updateService = serviceProvider.GetService<UpdateFieldService>();
 var service = serviceProvider.GetService<PlayGroundService>();
 
-if (service == null) return;
 
-//await service.UpdateAsync();
-await service.GetEntitiesAsync();
+//if (updateService != null)
+//    await updateService.UpdateAsync();
+
+if (service != null)
+    await service.GetEntitiesAsync();
