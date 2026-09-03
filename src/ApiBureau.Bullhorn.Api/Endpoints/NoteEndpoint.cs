e28c882fd5273ee1673d39059d7b5570b588fcaa
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2026-09-03 11:19:11 +0100

    refactor: remove obsolete date-based note search methods
    - Removed GetFromAsync, GetNewAndUpdatedFromAsync, and GetFromToAsync
    - These were replaced by SearchFromAsync, SearchNewAndUpdatedFromAsync, and SearchFromToAsync

--- src/ApiBureau.Bullhorn.Api/Endpoints/NoteEndpoint.cs
+++ src/ApiBureau.Bullhorn.Api/Endpoints/NoteEndpoint.cs
@@ -15,30 +15,6 @@ public class NoteEndpoint : SearchEndpointBase<NoteDto>
     public async Task<Result<ChangeResponse>> AddAsync(NoteDto dto, CancellationToken token)
         => await ApiConnection.PutAsJsonAsync(EntityType.Note, dto, token);
 
-    [Obsolete("Use SearchFromAsync", true)]
-    public async Task<List<NoteDto>> GetFromAsync(DateTime dateTime, CancellationToken token)
-    {
-        var query = $"{RequestUrl}?fields={AllFields}&query=dateAdded:[{dateTime:yyyyMMddHHmmss} TO *]";
-
-        return await ApiConnection.SearchAsync<NoteDto>(query, token: token);
-    }
-
-    [Obsolete("Use SearchNewAndUpdatedFromAsync", true)]
-    public async Task<List<NoteDto>> GetNewAndUpdatedFromAsync(DateTime dateTime, CancellationToken token)
-    {
-        var query = $"{RequestUrl}?fields={AllFields}&query=dateAdded:[{dateTime:yyyyMMddHHmmss} TO *] OR dateLastModified:[{dateTime:yyyyMMddHHmmss} TO *]";
-
-        return await ApiConnection.SearchAsync<NoteDto>(query, token: token) ?? new List<NoteDto>();
-    }
-
-    [Obsolete("Use SearchFromToAsync", true)]
-    public async Task<List<NoteDto>> GetFromToAsync(DateTime dateTimeFrom, DateTime dateTimeTo, CancellationToken token)
-    {
-        var query = $"{RequestUrl}?fields={AllFields}&query=dateAdded:[{dateTimeFrom:yyyyMMddHHmmss} TO {dateTimeTo:yyyyMMddHHmmss}]";
-
-        return await ApiConnection.SearchAsync<NoteDto>(query, token: token);
-    }
-
     public async Task<List<NoteDto>> GetNotesAsync(string userQuery, string fields = AllFields, CancellationToken token = default)
     {
         var query = $"{RequestUrl}?fields={fields}&query={userQuery}&sort=noteID"; // case must be noteID
